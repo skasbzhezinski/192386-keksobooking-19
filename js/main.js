e858e64942e240cff35bc835c0b84df24c308b1f
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-01-26 20:28:21 +0300

    add and connect the js/main.js file. create a layout of the similarAds object

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var similarAds = [
+  {
+    author: {
+      avatar: 'img/avatars/user**{{xx}}**.png'
+    } // где {{xx}} это число от 1 до 8 с ведущим нулём.
+    // Например, 01, 02 и т. д. Адреса изображений не повторяются
+  },
+  {
+    offer: {
+      title: 'BLAH!', // строка, заголовок предложения
+      address: '', // строка, адрес предложения. Для простоты пусть пока представляет собой
+      // запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
+      price: num, // число, стоимость
+      type: '', // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+      rooms: num, // число, количество комнат
+      guests: num, // число, количество гостей, которое можно разместить
+      checkin: '', // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+      checkout: '', // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+      features: '', // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher",
+      // "parking", "washer", "elevator", "conditioner",
+      description: '', // строка с описанием
+      photos: '', // массив строк случайной длины, содержащий адреса фотографий
+      // "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+      // "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+      // "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+    }
+  },
+  {
+    location: {
+      x: num, // случайное число, координата x метки на карте.
+      // Значение ограничено размерами блока, в котором перетаскивается метка
+      y: num, // случайное число, координата y метки на карте от 130 до 630
+    }
+  }
+];
+
+// ============== ОТЛАДКА ============== //
+console.log(similarAds[1].offer.title);
+
+// var map = document.querySelector('.map');
+// map.classList.remove('.map--faded');
