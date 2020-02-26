ae7e0ede022682caeb035e2eb3404e919e18a1f2
Skasbzhezinski <skasbzhezinski@gmail.com>
2020-02-26 20:19:08 +0300

    add pin.js module and connect it

--- js/map.js
+++ js/map.js
@@ -1,31 +1,6 @@
 'use strict';
 
 (function () {
-  var renderAds = function (adsArray) {
-    var userPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-    var userPin = userPinTemplate.cloneNode(true);
-    var userPinImg = userPin.querySelector('img');
-
-    userPin.style.left = adsArray.location.x - window.data.PIN_WIDTH / 2 + 'px';
-    userPin.style.top = adsArray.location.y + 'px';
-    userPinImg.src = adsArray.author.avatar;
-    userPinImg.alt = 'Заголовок объявления';
-
-    return userPin;
-  };
-
-  var addElement = function (elementsArray) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < elementsArray.length; i++) {
-      fragment.appendChild(renderAds(elementsArray[i]));
-    }
-    return fragment;
-  };
-
-  var insertElements = function () {
-    window.data.mapPins.appendChild(addElement(window.data.mock));
-  };
-
   var adMap = document.querySelector('.map');
   var notice = document.querySelector('.notice');
 
@@ -49,7 +24,7 @@
 
   // функция активации страницы
   var activate = function () {
-    insertElements();
+    window.insertElements();
 
     adForm.classList.remove('ad-form--disabled');
     adMap.classList.remove('map--faded');
---
+++ js/pin.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+  var renderAds = function (adsArray) {
+    var userPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+    var userPin = userPinTemplate.cloneNode(true);
+    var userPinImg = userPin.querySelector('img');
+
+    userPin.style.left = adsArray.location.x - window.data.PIN_WIDTH / 2 + 'px';
+    userPin.style.top = adsArray.location.y + 'px';
+    userPinImg.src = adsArray.author.avatar;
+    userPinImg.alt = 'Заголовок объявления';
+
+    return userPin;
+  };
+
+  var addElement = function (elementsArray) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < elementsArray.length; i++) {
+      fragment.appendChild(renderAds(elementsArray[i]));
+    }
+    return fragment;
+  };
+
+  var insertElements = function () {
+    window.data.mapPins.appendChild(addElement(window.data.mock));
+  };
+
+  window.insertElements = insertElements;
+})();
